22d794feb4ae1dc28d7c4c57038405feeeadad4b
Juan Manuel Perez <jm.perezs1@uniandes.edu.co>
2024-10-02 08:31:12 -0500

    Correccion

--- application_dart/macos/Flutter/GeneratedPluginRegistrant.swift
+++ application_dart/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,13 +7,7 @@ import Foundation
 
 import cloud_firestore
 import firebase_auth
-import geolocator_apple
 import firebase_core
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
-  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
 import geolocator_apple
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
